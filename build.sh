9e353cc2a0643852632fb419f8b6de98944ca568
Keitaro Murakami <keitaro.mrkm@gmail.com>
2024-02-15 17:16:09 +0900

    <WIP>[build.sh] don't clear build directory and build only this package

--- build.sh
+++ build.sh
@@ -25,11 +25,11 @@ else
 fi
 echo "ROS version is: "$ROS_VERSION
 
-# clear `build/` folder.
-# TODO: Do not clear these folders, if the last build is based on the same ROS version.
-rm -rf ../../build/
-rm -rf ../../devel/
-rm -rf ../../install/
+# # clear `build/` folder.
+# # TODO: Do not clear these folders, if the last build is based on the same ROS version.
+# rm -rf ../../build/
+# rm -rf ../../devel/
+# rm -rf ../../install/
 # clear src/CMakeLists.txt if it exists.
 if [ -f ../CMakeLists.txt ]; then
     rm -f ../CMakeLists.txt
@@ -55,7 +55,7 @@ fi
 pushd `pwd` > /dev/null
 if [ $ROS_VERSION = ${VERSION_ROS1} ]; then
     cd ../../
-    catkin build --cmake-args -DROS_EDITION=${VERSION_ROS1}
+    catkin build livox_ros_driver2 --cmake-args -DROS_EDITION=${VERSION_ROS1}
 elif [ $ROS_VERSION = ${VERSION_ROS2} ]; then
     cd ../../
     colcon build --cmake-args -DROS_EDITION=${VERSION_ROS2} -DHUMBLE_ROS=${ROS_HUMBLE}
